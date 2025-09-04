fc30e8dd53bb56d8a4217d01148b122287e8c4cb
manula-xigenix <git config --global user.email>
2025-09-04 10:42:09 +0530

    CleanUp The Code

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,4 +1,3 @@
-// src/app/core/guards/auth.guard.ts
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
